bc4e116fca9e4f9530a8f1416c9502879b616727
l <495462460@qq.com>
2019-10-09 10:53:02 +0800

    update test01

--- src/com/eju/hellloproject/HelloProject.java
+++ src/com/eju/hellloproject/HelloProject.java
@@ -2,7 +2,7 @@ package com.eju.hellloproject;
 
 public class HelloProject {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test 01");
 
         System.out.printf("Hello idea!");
 
