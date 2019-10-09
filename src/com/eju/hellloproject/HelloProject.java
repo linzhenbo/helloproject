38e86290c9588e5e99100ef6fa7aaece3f6892d5
l <495462460@qq.com>
2019-10-09 10:31:05 +0800

    f

---
+++ src/com/eju/hellloproject/HelloProject.java
@@ -0,0 +1,9 @@
+package com.eju.hellloproject;
+
+public class HelloProject {
+    public static void main(String[] args) {
+        System.out.println("test");
+
+        System.out.printf("Hello idea!");
+    }
+}
